74da1cb5be9915d0e1b03a42fbcabcf040a255c5
Michael Ilkanayev <michaelilkanayev@gmail.com>
2025-10-07 11:11:41 +0300

    add Tab component

---
+++ components/Tab/Tab.tsx
@@ -0,0 +1,65 @@
+import { FC, useRef, useState } from 'react';
+import { Pressable, StyleSheet, Text } from 'react-native';
+import { getFontFamily } from '../../assets/fonts/helper';
+import {
+  horizontalScale,
+  scaleFontSize,
+  verticalScale,
+} from '../../assets/styles/scaling';
+import { event } from 'react-native/types_generated/Libraries/Animated/AnimatedExports';
+
+interface TabProps {
+  title?: string;
+  isInactive?: boolean;
+  onPress: () => void;
+}
+
+const Tab: FC<TabProps> = ({ title = '', isInactive = false, onPress }) => {
+  const [width, setWidth] = useState(0);
+  const textRef = useRef<Text>(null);
+  const paddingHorizontal = 33;
+  const tabWidth = { width: horizontalScale(paddingHorizontal * 2 + width) };
+
+  return (
+    <Pressable
+      disabled={isInactive}
+      style={[styles.tab, isInactive && styles.inactiveTab, tabWidth]}
+      onPress={() => onPress()}
+    >
+      <Text
+        onTextLayout={event => {
+          const { width } = event.nativeEvent.lines[0];
+          setWidth(width);
+        }}
+        ref={textRef}
+        style={[styles.title, isInactive && styles.inactiveTitle]}
+      >
+        {title}
+      </Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  tab: {
+    backgroundColor: '#2979F2',
+    height: verticalScale(50),
+    justifyContent: 'center',
+    borderRadius: horizontalScale(50),
+  },
+  title: {
+    fontFamily: getFontFamily('Inter', '500'),
+    fontSize: scaleFontSize(14),
+    lineHeight: scaleFontSize(17),
+    color: '#FFFFFF',
+    textAlign: 'center',
+  },
+  inactiveTab: {
+    backgroundColor: '#F3F5F9',
+  },
+  inactiveTitle: {
+    color: '#79869F',
+  },
+});
+
+export default Tab;
--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -4,12 +4,15 @@ import { SafeAreaView, StyleSheet } from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
+import Tab from '../../components/Tab/Tab';
 
 const Home: FC = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title="Michael Magic" type={1} />
-      <Button title="Donate" isDisabled={false} />
+      <Tab title="Active" isInactive={false} onPress={() => {}} />
+      <Tab title="Inactive" isInactive onPress={() => {}} />
+      <Button title="Press Me" onPress={() => {}} />
     </SafeAreaView>
   );
 };
