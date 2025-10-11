ef2a97b2b80b3f7942df5cd0b17fd94f422cc274
Michael Ilkanayev <michaelilkanayev@gmail.com>
2025-10-11 13:34:43 +0300

    feat: integrate redux-persist for state management and add AsyncStorage support

--- redux/store.ts
+++ redux/store.ts
@@ -1,16 +1,36 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { logger } from 'redux-logger';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
 
 import userReducer from './reducers/User';
 
+// Combine all your reducers
 const rootReducer = combineReducers({
   user: userReducer,
 });
 
+// Configure redux-persist
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  version: 1,
+};
+
+// Wrap rootReducer with persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create the store using the persisted reducer
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false, // required for redux-persist
+    }).concat(logger),
 });
+
+// Create the persistor
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
