5ef609c0d16fe3520a3513f6a18204f1a0c68187
Michael Ilkanayev <michaelilkanayev@gmail.com>
2025-10-14 14:48:04 +0300

    feat: add Login screen

--- assets/styles/globalStyle.js
+++ assets/styles/globalStyle.js
@@ -1,11 +1,16 @@
 import { StyleSheet } from 'react-native';
 
+import { verticalScale } from './scaling';
+
 const styles = StyleSheet.create({
   backgroundWhite: {
     backgroundColor: '#FFFFFF',
   },
   flex: {
     flex: 1,
+  },
+  marginBottom24: {
+    marginBottom: verticalScale(24),
   },
 });
 
---
+++ screens/Login/Login.tsx
@@ -0,0 +1,71 @@
+import { FC, useState } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  Pressable,
+  View,
+  StyleSheet,
+} from 'react-native';
+
+import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
+import globalStyle from '../../assets/styles/globalStyle';
+import { horizontalScale } from '../../assets/styles/scaling';
+import Input from '../../components/Input/Input';
+
+const Login: FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  return (
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.container}
+      >
+        <View style={globalStyle.marginBottom24}>
+          <Header type={1} title="Welcome Back" />
+        </View>
+
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            keyboardType="email-address"
+            label="Email"
+            placeholder="Enter your email..."
+            onChangeText={setEmail}
+          />
+        </View>
+
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="******"
+            onChangeText={setPassword}
+          />
+        </View>
+
+        <View style={globalStyle.marginBottom24}>
+          <Button title="Login" onPress={() => {}} />
+        </View>
+
+        <Pressable style={styles.registrationButton}>
+          <Header color="#156CF7" type={3} title="Don't have an account?" />
+        </Pressable>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: horizontalScale(24),
+    flex: 1,
+    justifyContent: 'center',
+  },
+  registrationButton: {
+    alignItems: 'center',
+  },
+});
+
+export default Login;
